aabcb7f5533b7ef47ed0b32c0c07f3c3d069dfaf
Tobias McNulty <tobias@caktusgroup.com>
2019-03-29 14:03:27 -0400

    add docker-entrypoint.sh from 2017 post

---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+until psql $DATABASE_URL -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - continuing"
+
+if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
+    /venv/bin/python manage.py migrate --noinput
+Fi
+
+if [ "x$DJANGO_MANAGEPY_COLLECTSTATIC" = 'xon' ]; then
+    /venv/bin/python manage.py collectstatic --noinput
+fi
+
+exec "$@"

e3268d5cd4c260c31427c4ae4ae58bf66e58ed6f
Tobias McNulty <tobias@caktusgroup.com>
2019-03-29 22:27:35 -0400

    shouldn't need to run collectstatic in docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,8 +12,4 @@ if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
     /venv/bin/python manage.py migrate --noinput
 fi
 
-if [ "x$DJANGO_MANAGEPY_COLLECTSTATIC" = 'xon' ]; then
-    /venv/bin/python manage.py collectstatic --noinput
-fi
-
 exec "$@"

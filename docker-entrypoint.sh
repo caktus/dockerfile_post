ef118c3a21921d15957178b307dacb279fcec54b
Tobias McNulty <tobias@caktusgroup.com>
2019-03-29 23:02:54 -0400

    correct indentation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 until psql $DATABASE_URL -c '\l'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
+    >&2 echo "Postgres is unavailable - sleeping"
+    sleep 1
 done
 
 >&2 echo "Postgres is up - continuing"

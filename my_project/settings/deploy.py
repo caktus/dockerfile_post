36f49a79a6fb5cd4f688422969fe0b0e939d97ea
Tobias McNulty <tobias@caktusgroup.com>
2019-03-29 22:42:02 -0400

    more cleanup

--- my_project/settings/deploy.py
+++ my_project/settings/deploy.py
@@ -1,12 +1,18 @@
-from . import *
+import os
+
 import dj_database_url
+
+from . import *  # noqa: F403
+
+# This is NOT a complete production settings file. For more, see:
+# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['localhost']
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)  # noqa: F405
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # noqa: F405
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
